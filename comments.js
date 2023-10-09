d2aad2b2f55c9256f6088e7754400b36fbe56e09
Shakhzod Sharifov <thedigitaluzb@gmail.com>
2023-10-09 16:53:27 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create webserver 
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comment');
+var Post = require('../models/post');
+var User = require('../models/user');
+
+// Create a comment
+router.post('/', function(req, res) {
+    var comment = new Comment({
+        content: req.body.content,
